9d0acd3e5b74779cb8364d5098304ba710a813ca
Sathish <sathish316@gmail.com>
2013-01-05 13:35:09 +0530

    Emacs starter kit initial commit

---
+++ elpa/color-theme-x-1.3/color-theme-x-autoloads.el
@@ -0,0 +1,18 @@
+;;; color-theme-x-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("color-theme-x-pkg.el" "color-theme-x.el")
+;;;;;;  (20711 56320 619260))
+
+;;;***
+
+(provide 'color-theme-x-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; color-theme-x-autoloads.el ends here
